fe3bfba74c3375f9ee563aefa1fc9921003ae889
Manas Tanneeru <tmanas9@outlook.com>
2024-07-12 23:39:11 -0700

    Final First Commit
    Hopefully

---
+++ js/datetime.js
@@ -0,0 +1,16 @@
+function getCurrentDate() {
+    var currentDate = new Date();
+    var options = { month: 'short' };
+    var day = currentDate.getDate();
+    var month = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+    var year = currentDate.getFullYear();
+
+    return `${month} ${day}, ${year}`;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var dateElement = document.querySelector(".fh5co-blog .posted_on");
+    if (dateElement) {
+        dateElement.textContent = getCurrentDate();
+    }
+});
