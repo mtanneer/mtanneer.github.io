e698bbbb3bdd157798228b910f6cd2ad2d1fde20
Manas Tanneeru <tmanas9@outlook.com>
2024-07-13 23:10:37 -0700

    added a projects page

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     typeWriter();
 });
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            modals[i].style.display = 'none';
+        }
+    }
+}
